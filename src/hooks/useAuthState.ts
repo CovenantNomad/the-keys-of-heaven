2947ddbd5e27808460ccbb213ef23ba381667a02
Jeonghun Nam <adonis0951@hotmail.com>
2023-01-04 15:26:58 +0900

    fix: @useAuthState typeError - UserType is not imported

--- src/hooks/useAuthState.ts
+++ src/hooks/useAuthState.ts
@@ -1,6 +1,7 @@
 import { onAuthStateChanged } from 'firebase/auth'
 import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { auth } from 'src/config/firebaseConfig'
+import { UserType } from 'src/types/types'
 
 function useAuthState(): [
   UserType | null,
